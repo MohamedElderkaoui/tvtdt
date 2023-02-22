a2edf25ba3c0e1af4ea8eb14e1fa7d541a5a511e
mohamed <mderkaoui10@gmail.com>
2023-02-22 09:19:10 +0100

    fc

--- tvttdt.py
+++ tvttdt.py
@@ -4,20 +4,56 @@ import json
 
 # URL con la lista de canales
 URL = 'https://www.tdtchannels.com/lists/tv.json'
-res = requests.get(URL)
-data = res.json()
 
+
+@st.cache_data
 def obtener_datos(URL):
     res = requests.get(URL)
     data = res.json()
     return data
+
+
+data = obtener_datos(URL)
 # guardar json
 # with open('tdt.json', 'w') as f:
 #     json.dump(data, f)
 
-def country(data):
-    [c['name '] for c in data['countries']]
 
-st.title("TDT")
+def get_countries(data):
+    return [c['name'] for c in data['countries']]
 
-st.bhc
+
+def get_ambitos(data, country):
+    pais = [a for a in data['countries'] if a['name'] == country][0]
+    return [a['name'] for a in pais['ambits']]
+
+
+def get_url(options):
+    '''Devuelve la url del primer canal de la lista, si existe'''
+    if options:
+        return options[0]['url']
+    else:
+        return ''
+
+
+def get_canales(data, country, ambito):
+    pais = [a for a in data['countries'] if a['name'] == country][0]
+    ambito = [a for a in pais['ambits'] if a['name'] == ambito][0]
+    # Devolver lista de dict: nombre, url y logo
+    return [{'name': c['name'],
+             'url': get_url(c['options']),
+             'logo': c['logo']} for c in ambito['channels']]
+
+
+st.title("AplicaciÃ³n canales TDT")
+
+countries = get_countries(data)
+
+zona = st.radio('Selecciona Zona', countries)
+ambitos = get_ambitos(data, zona)
+ambito = st.selectbox('Selecciona Ambito', ambitos)
+
+# st.write(get_canales(data, zona, ambito))
+canales = get_canales(data, zona, ambito)
+for c in canales:
+    st.markdown(f'[{c["name"]}]({c["url"]})')
